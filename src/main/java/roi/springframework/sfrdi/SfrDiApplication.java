6ea18e862217605e265ddc3a0eee442b7e46af73
roibar81 <baraziroi@gmail.com>
2021-12-22 16:09:58 +0200

    Adding controllers package. Adding MyController class, and annotating @Controller. Setting the SpringApplication.run to ApplicationContext object as ctx. Creating MyController Objects with Spring(without using 'new') and using his very simple functionality.

--- src/main/java/roi/springframework/sfrdi/SfrDiApplication.java
+++ src/main/java/roi/springframework/sfrdi/SfrDiApplication.java
@@ -2,12 +2,21 @@ package roi.springframework.sfrdi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import roi.springframework.sfrdi.controllers.MyController;
 
 @SpringBootApplication
 public class SfrDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfrDiApplication.class, args);
+
+		ApplicationContext ctx = SpringApplication.run(SfrDiApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);
 	}
 
 }
---
+++ src/main/java/roi/springframework/sfrdi/controllers/MyController.java
@@ -0,0 +1,13 @@
+package roi.springframework.sfrdi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello(){
+        System.out.println("Hello world");
+
+        return "Hi Roi";
+    }
+}
