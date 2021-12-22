c119ce1fcd54e41701fd2c1144e9bcf197c893bb
roibar81 <baraziroi@gmail.com>
2021-12-22 14:48:35 +0200

    Project Initialize

---
+++ src/test/java/roi/springframework/sfrdi/SfrDiApplicationTests.java
@@ -0,0 +1,13 @@
+package roi.springframework.sfrdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfrDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
