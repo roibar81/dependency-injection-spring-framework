84d531e73a0efbb67ccd950840637373ca82af89
roibar81 <baraziroi@gmail.com>
2021-12-25 23:33:16 +0200

    Adding PrimaryGreetingService class.

--- src/main/java/roi/springframework/sfrdi/controllers/MyController.java
+++ src/main/java/roi/springframework/sfrdi/controllers/MyController.java
@@ -1,13 +1,21 @@
 package roi.springframework.sfrdi.controllers;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Controller;
+import roi.springframework.sfrdi.serices.GreetingService;
 
 @Controller
 public class MyController {
 
-    public String sayHello(){
-        System.out.println("Hello world");
+    GreetingService greetingService;
 
-        return "Hi Roi";
+    //@Autowired is default with constructor.
+    // Because we do not @Qualifier annotate spring inject the serviceObject with the @Primary annotate.
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello(){
+        return greetingService.sayGreeting();
     }
 }
