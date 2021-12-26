1af9c40f5508202a893a6bb7bc32e5b284152c25
roibar81 <baraziroi@gmail.com>
2021-12-26 16:22:03 +0200

    Setting a default active Profile. Another way to set instead of setting that in the application.properties.

--- src/main/java/roi/springframework/sfrdi/serices/I18nSpanishGreetingService.java
+++ src/main/java/roi/springframework/sfrdi/serices/I18nSpanishGreetingService.java
@@ -3,7 +3,7 @@ package roi.springframework.sfrdi.serices;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService{
 
